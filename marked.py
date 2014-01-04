dd0f8dd0fc3b3c32c59f9b695f00fe06ff042a6d
Wes Mason <wes@1stvamp.org>
2014-01-04 01:47:44 +0000

    Iterate over the contents of both snippets and full documents

--- marked.py
+++ marked.py
@@ -1,1 +1,11 @@
 from bs4 import BeautifulSoup
+
+
+def markup_to_markdown(content):
+    soup = BeautifulSoup(content)
+
+    # Account for HTML snippets and full documents alike
+    contents = soup.body.contents if soup.body is not None else soup.contents
+
+    for c in contents:
+        pass
