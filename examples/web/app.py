947b78de315c05f5680f67be7ee6e4c6329fec0c
Wes Mason <wes@1stvamp.org>
2014-02-08 16:18:35 +0000

    Add example tornado app

---
+++ examples/web/app.py
@@ -0,0 +1,32 @@
+from tornado.ioloop import IOLoop
+from tornado.httpclient import AsyncHTTPClient
+from tornado.web import asynchronous, RequestHandler, Application
+
+from marked import markup_to_markdown
+
+
+class MainHandler(RequestHandler):
+    def prepare(self):
+        self.set_header('Content-Type', 'text/markdown; charset="utf-8"')
+
+    @asynchronous
+    def post(self):
+        client = AsyncHTTPClient()
+        client.fetch(
+            request=self.get_argument('url'),
+            method='GET',
+            callback=self.handle_response,
+        )
+        self.flush()
+
+    def handle_response(self, response):
+        self.finish(markup_to_markdown(response.body))
+
+
+application = Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    IOLoop.instance().start()
